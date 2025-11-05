1f88fed1516f4c3819e282717fe01593d0878032
Balagur Dmitry <farnitin@yandex.ru>
2025-11-05 04:31:07 +0400

    add realisation of class ClimateSensorEventDto

--- telemetry/collector/src/main/java/dto/ClimateSensorEventDto.java
+++ telemetry/collector/src/main/java/dto/ClimateSensorEventDto.java
@@ -1,4 +1,20 @@
 package dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.yandex.practicum.kafka.telemetry.event.ConditionType;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
 public class ClimateSensorEventDto extends SensorEventDto {
+    private int temperatureC;
+    private int humidity;
+    private int co2Level;
+
+    @Override
+    public ConditionType getType() {
+        return ConditionType.CLIMATE_SENSOR_EVENT;
+    }
 }
