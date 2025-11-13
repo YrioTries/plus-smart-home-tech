b68a6ed9144e33db61b899fc06a83502caf4ba91
Balagur Dmitry <farnitin@yandex.ru>
2025-11-13 13:33:42 +0400

    try to friend new producer with service

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventServiceIml.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventServiceIml.java
@@ -4,31 +4,29 @@ import ru.yandex.practicum.dto.hub.HubEventDto;
 import ru.yandex.practicum.dto.sensor.SensorEventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.kafka.KafkaEventProducer;
+import ru.yandex.practicum.kafka.config.TopicType;
 import ru.yandex.practicum.mapper.HubEventMapper;
 import ru.yandex.practicum.mapper.SensorEventMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.kafka.telemetry.event.HubEvent;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEvent;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class SensorEventServiceIml implements SensorEventService {
-    private final AvroEventProducerService avroEventProducerService;
+    private final KafkaEventProducer kafkaEventProducer;
     private final SensorEventMapper sensorEventMapper;
     private final HubEventMapper hubEventMapper;
 
-    @Transactional
-    public void processSensorEvent(SensorEventDto eventDto) {
-        SensorEvent sensorEvent = sensorEventMapper.toAvro(eventDto);
-        avroEventProducerService.sendSensorEvent(sensorEvent);
+    public void processSensorEvent(SensorEventDto sensorEventDto) {
+        SensorEvent sensorEvent = sensorEventMapper.toAvro(sensorEventDto);
+        kafkaEventProducer.send(sensorEvent, sensorEventDto.getHubId(), sensorEventDto.getTimestamp(), TopicType.TELEMETRY_SENSORS_V1);
     }
 
-    @Transactional
-    public void processHubEvent(HubEventDto eventDto) {
-        HubEvent hubEvent = hubEventMapper.toAvro(eventDto);
-        avroEventProducerService.sendHubEvent(hubEvent);
+    public void processHubEvent(HubEventDto hubEventDto) {
+        HubEvent hubEvent = hubEventMapper.toAvro(hubEventDto);
+        kafkaEventProducer.send(hubEvent, hubEventDto.getHubId(), hubEventDto.getTimestamp(), TopicType.TELEMETRY_SENSORS_V1);
     }
 }
