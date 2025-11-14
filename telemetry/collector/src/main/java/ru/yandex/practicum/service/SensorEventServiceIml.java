617251cb3685f9f2bb6c398ab8d3ad6a18234d4e
Balagur Dmitry <farnitin@yandex.ru>
2025-11-14 14:42:50 +0400

    add error catching in service

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventServiceIml.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/SensorEventServiceIml.java
@@ -22,22 +22,35 @@ public class SensorEventServiceIml implements SensorEventService {
     private final HubEventMapper hubEventMapper;
 
     public void processSensorEvent(SensorEventDto sensorEventDto) {
-        SensorEvent sensorEvent = sensorEventMapper.toAvro(sensorEventDto);
-        kafkaEventProducer.send(
-                sensorEvent,
-                sensorEvent.getHubId(),
-                Instant.ofEpochMilli(sensorEvent.getTimestamp()),
-                TopicType.TELEMETRY_SENSORS_V1
-        );
+        log.info("Processing sensor event: {}", sensorEventDto);
+        try {
+            SensorEvent sensorEvent = sensorEventMapper.toAvro(sensorEventDto);
+            kafkaEventProducer.send(
+                    sensorEvent,
+                    sensorEvent.getHubId(),
+                    Instant.ofEpochMilli(sensorEvent.getTimestamp()),
+                    TopicType.TELEMETRY_SENSORS_V1
+            );
+        } catch (Exception e) {
+            log.error("Error processing sensor event: {}", sensorEventDto, e);
+            throw e;
+        }
     }
 
     public void processHubEvent(HubEventDto hubEventDto) {
-        HubEvent hubEvent = hubEventMapper.convertHubToAvro(hubEventDto);
-        kafkaEventProducer.send(
-                hubEvent,
-                hubEvent.getHubId(),
-                Instant.ofEpochMilli(hubEvent.getTimestamp()),
-                TopicType.TELEMETRY_HUBS_V1
-        );
+        log.info("Processing hub event: {}", hubEventDto);
+        try {
+            HubEvent hubEvent = hubEventMapper.convertHubToAvro(hubEventDto);
+            kafkaEventProducer.send(
+                    hubEvent,
+                    hubEvent.getHubId(),
+                    Instant.ofEpochMilli(hubEvent.getTimestamp()),
+                    TopicType.TELEMETRY_HUBS_V1
+            );
+        } catch (Exception e) {
+            log.error("Error processing hub event: {}", hubEventDto, e);
+            throw e;
+        }
+
     }
 }
