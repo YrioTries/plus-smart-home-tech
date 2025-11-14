b72ff367794d602007457104ce142e25f8c74724
Balagur Dmitry <farnitin@yandex.ru>
2025-11-14 14:22:31 +0400

    add logging in controller

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/SensorController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/SensorController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.dto.hub.HubEventDto;
 import ru.yandex.practicum.dto.sensor.SensorEventDto;
 import jakarta.validation.Valid;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.service.SensorEventService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events")
@@ -17,12 +19,14 @@ public class SensorController {
     @PostMapping("/sensors")
     @ResponseStatus(HttpStatus.CREATED)
     public void collectSensorEvent(@Valid @RequestBody SensorEventDto event) {
+        log.info("Received sensor event: {}", event);
         sensorEventService.processSensorEvent(event);
     }
 
     @PostMapping("/hubs")
     @ResponseStatus(HttpStatus.CREATED)
     public void collectHubEvent(@Valid @RequestBody HubEventDto event) {
+        log.info("Received hub event: {}", event);
         sensorEventService.processHubEvent(event);
     }
 }
