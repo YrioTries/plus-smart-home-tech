42ac151e830d4798aa2bbbfa6562eee509cad5a8
Balagur Dmitry <farnitin@yandex.ru>
2025-11-16 15:39:39 +0400

    fix config

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/config/KafkaConfig.java
@@ -18,23 +18,22 @@ public class KafkaConfig {
 
     @PostConstruct
     public void init() {
-        //Producer Config
+        // Producer Config
         this.producerConfig = new Properties();
         this.producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        this.producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.VoidSerializer");
+        this.producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"); // ← ИСПРАВЛЕНО
         this.producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
+        this.producerConfig.put(ProducerConfig.ACKS_CONFIG, "1");
+        this.producerConfig.put(ProducerConfig.LINGER_MS_CONFIG, 5);
+        this.producerConfig.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        this.producerConfig.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
 
-
-        //Consumer Config
+        // Consumer Config
         this.consumerConfig = new Properties();
-        this.consumerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        this.consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // ← ИСПРАВЛЕНО
         this.consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         this.consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GeneralAvroDeserializer.class.getName());
         this.consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "collector-group");
         this.consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        this.producerConfig.put(ProducerConfig.ACKS_CONFIG, "1"); // Подтверждение от лидера
-        this.producerConfig.put(ProducerConfig.LINGER_MS_CONFIG, 5); // Задержка для батчинга
-        this.producerConfig.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384); // Размер батча
-        this.producerConfig.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432); // Память для буфера
     }
 }
