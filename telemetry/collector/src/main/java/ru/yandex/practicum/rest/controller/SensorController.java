41e835d4dc2c3e9efd61ed60db755209b2143242
Balagur Dmitry <farnitin@yandex.ru>
2025-11-24 21:33:00 +0400

    fix SensorController

--- telemetry/collector/src/main/java/ru/yandex/practicum/rest/controller/SensorController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/rest/controller/SensorController.java
@@ -1,32 +1,32 @@
 package ru.yandex.practicum.rest.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.rest.dto.hub.HubEventDto;
-import ru.yandex.practicum.rest.dto.sensor.SensorEventDto;
+import ru.yandex.practicum.dto.hub.HubEventDto;
+import ru.yandex.practicum.dto.sensor.SensorEventDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.rest.service.SensorEventService;
+import ru.yandex.practicum.service.CollectorEventService;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events")
 public class SensorController {
-    private final SensorEventService sensorEventService;
+    private final CollectorEventService collectorEventService;
 
     @PostMapping("/sensors")
     @ResponseStatus(HttpStatus.CREATED)
     public void collectSensorEvent(@Valid @RequestBody SensorEventDto event) {
         log.info("Received sensor event: {}", event);
-        sensorEventService.processSensorEvent(event);
+        collectorEventService.processSensorEvent(event);
     }
 
     @PostMapping("/hubs")
     @ResponseStatus(HttpStatus.CREATED)
     public void collectHubEvent(@Valid @RequestBody HubEventDto event) {
         log.info("Received hub event: {}", event);
-        sensorEventService.processHubEvent(event);
+        collectorEventService.processHubEvent(event);
     }
 }
