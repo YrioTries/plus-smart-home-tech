356f946361823861cbf8206f72afdf4a750dd9e4
Balagur Dmitry <farnitin@yandex.ru>
2025-11-25 01:51:17 +0400

    add AnalyzerApplication

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.processor.HubEventProcessor;
+import ru.yandex.practicum.processor.SnapshotProcessor;
+
+@Slf4j
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class AnalyzerApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
+
+        final HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
+        ru.yandex.practicum.processor.SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
+
+        Thread hubEventsThread = new Thread(hubEventProcessor);
+        hubEventsThread.setName("HubEventHandlerThread");
+        hubEventsThread.start();
+
+        snapshotProcessor.start();
+
+        log.info("Analyzer application started successfully");
+    }
+}
