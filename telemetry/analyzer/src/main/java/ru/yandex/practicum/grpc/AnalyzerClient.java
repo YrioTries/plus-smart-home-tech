f85b94cd962bef7438200f61fd6207afac7a70f7
Balagur Dmitry <farnitin@yandex.ru>
2025-11-27 06:15:05 +0400

    add delay to AnalyzerClient

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/grpc/AnalyzerClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/grpc/AnalyzerClient.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.grpc;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.messages.DeviceActionRequest;
@@ -12,13 +14,28 @@ public class AnalyzerClient {
 
     private final HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient;
 
+    @Autowired
     public AnalyzerClient(@GrpcClient("hub-router")
                           HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient) {
         this.hubRouterClient = hubRouterClient;
     }
 
+    @PostConstruct
+    public void init() {
+        log.info("AnalyzerClient initialized with gRPC stub: {}", hubRouterClient != null);
+    }
+
     public void sendDeviceActions(DeviceActionRequest request) {
-        log.info("Sending action to hub {} for scenario {}", request.getHubId(), request.getScenarioName());
-        hubRouterClient.handleDeviceAction(request);
+        if (hubRouterClient == null) {
+            log.error("gRPC client is null! Check gRPC configuration.");
+            return;
+        }
+
+        try {
+            log.info("Sending action to hub {} for scenario {}", request.getHubId(), request.getScenarioName());
+            hubRouterClient.handleDeviceAction(request);
+        } catch (Exception e) {
+            log.error("Failed to send action to hub {}: {}", request.getHubId(), e.getMessage());
+        }
     }
 }
