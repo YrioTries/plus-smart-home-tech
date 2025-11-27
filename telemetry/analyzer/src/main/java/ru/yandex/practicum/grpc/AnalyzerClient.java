6cfa4a9bd36b24096f6e5aef58f89e5328de2a24
Balagur Dmitry <farnitin@yandex.ru>
2025-11-27 13:11:48 +0400

    try return @Autowired(required = false

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/grpc/AnalyzerClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/grpc/AnalyzerClient.java
@@ -1,9 +1,8 @@
 package ru.yandex.practicum.grpc;
 
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.messages.DeviceActionRequest;
@@ -14,10 +13,12 @@ public class AnalyzerClient {
 
     private final HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient;
 
-    public AnalyzerClient(@GrpcClient("hub-router") HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient) {
+    public AnalyzerClient(@GrpcClient("hub-router")
+                          @Autowired(required = false)
+                          HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient) {
         this.hubRouterClient = hubRouterClient;
         if (hubRouterClient == null) {
-            log.warn("gRPC клиент не инициализирован - hub-router недоступен. Analyzer будет работать без отправки команд.");
+            log.warn("gRPC клиент не инициализирован - hub-router не доступен. Analyzer будет работать без отправки команд.");
         } else {
             log.info("gRPC клиент успешно инициализирован");
         }
@@ -25,33 +26,10 @@ public class AnalyzerClient {
 
     public void sendDeviceActions(DeviceActionRequest request) {
         if (hubRouterClient == null) {
-            log.warn("gRPC клиент недоступен, пропускаем отправку действия для хаба: {}, сценарий: {}, устройство: {}",
-                    request.getHubId(),
-                    request.getScenarioName(),
-                    request.getAction().getSensorId());
+            log.warn("gRPC клиент не доступен, пропускаем отправку действия для хаба {}", request.getHubId());
             return;
         }
-
-        try {
-            log.info("Отправка команды в hub-router: хаб={}, сценарий={}, устройство={}, действие={}",
-                    request.getHubId(),
-                    request.getScenarioName(),
-                    request.getAction().getSensorId(),
-                    request.getAction().getType());
-
-            var response = hubRouterClient.handleDeviceAction(request);
-            log.debug("Команда успешно отправлена в hub-router: {}", response);
-
-        } catch (StatusRuntimeException e) {
-            if (e.getStatus().getCode() == Status.Code.UNAVAILABLE) {
-                log.error("Hub Router недоступен на localhost:59090. Убедитесь, что сервис запущен");
-            } else {
-                log.error("Ошибка при отправке команды в hub-router. Статус: {}, Описание: {}",
-                        e.getStatus().getCode(), e.getStatus().getDescription(), e);
-            }
-        } catch (Exception e) {
-            log.error("Неожиданная ошибка при отправке команды в hub-router для хаба: {}, сценарий: {}",
-                    request.getHubId(), request.getScenarioName(), e);
-        }
+        log.info("Sending action to hub {} for scenario {}", request.getHubId(), request.getScenarioName());
+        hubRouterClient.handleDeviceAction(request);
     }
 }
