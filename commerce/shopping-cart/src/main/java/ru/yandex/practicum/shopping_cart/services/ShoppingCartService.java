648000ebfc647dd69d37cbebf9d57d66c441f893
Y7tion <farnitin@yandex.ru>
2025-12-19 19:48:10 +0400

    add ShoppingCartService

---
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/services/ShoppingCartService.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.shopping_cart.services;
+
+import ru.yandex.practicum.interaction_api.model.dto.ProductDto;
+import ru.yandex.practicum.interaction_api.model.dto.ShoppingCartDto;
+import ru.yandex.practicum.interaction_api.model.dto.request.ChangeProductQuantityRequest;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+
+    ShoppingCartDto getCurrentSoppingCart(String username);
+
+    ShoppingCartDto removeFromShoppingCart(String username, List<String> productIds);
+
+    ShoppingCartDto changeProductQuantity(String username, ChangeProductQuantityRequest request);
+
+    ShoppingCartDto addInShoppingCart(String username, List<ProductDto> productList);
+
+    void deactivateShoppingCart(String username);
+}
