c20efb869e66de5d2eaf2d6a1c21a016daf9603d
Y7tion <farnitin@yandex.ru>
2025-12-19 20:15:45 +0400

    add WarehouseController realisation

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/controllers/WarehouseController.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/controllers/WarehouseController.java
@@ -8,31 +8,36 @@ import ru.yandex.practicum.interaction_api.model.dto.BookedProductsDto;
 import ru.yandex.practicum.interaction_api.model.dto.ShoppingCartDto;
 import ru.yandex.practicum.interaction_api.model.dto.request.AddProductToWarehouseRequest;
 import ru.yandex.practicum.interaction_api.model.dto.request.NewProductInWarehouseRequest;
+import ru.yandex.practicum.warehouse.services.WarehouseService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/warehouse")
 public class WarehouseController {
 
+    private final WarehouseService warehouseService;
+
     @GetMapping("/address")
     @ResponseStatus(HttpStatus.OK)
     public AddressDto getAddress() {
-        return new AddressDto();
+        return warehouseService.getAddress();
     }
 
     @PostMapping("/check")
     @ResponseStatus(HttpStatus.OK)
     public BookedProductsDto checkProductsWarehouse(@RequestBody ShoppingCartDto shoppingCartDto) {
-        return new BookedProductsDto();
+        return warehouseService.checkProductsWarehouse(shoppingCartDto);
     }
 
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.OK)
     public void acceptProductToWareHouse(@RequestBody AddProductToWarehouseRequest request) {
+        warehouseService.acceptProductToWareHouse(request);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
     public void addProductToWareHouse(@RequestBody NewProductInWarehouseRequest request) {
+        warehouseService.addProductToWareHouse(request);
     }
 }
