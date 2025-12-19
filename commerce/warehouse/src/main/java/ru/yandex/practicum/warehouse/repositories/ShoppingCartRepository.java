3d236cdded1893575459d95cef431c45e877dcfb
Y7tion <farnitin@yandex.ru>
2025-12-20 00:15:08 +0400

    add ShoppingCartRepository

---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/repositories/ShoppingCartRepository.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.warehouse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.yandex.practicum.interaction_api.model.entity.ShoppingCartEntity;
+
+import java.util.Optional;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCartEntity, String> {
+
+    Optional<ShoppingCartEntity> findByOwner(String owner);
+}
