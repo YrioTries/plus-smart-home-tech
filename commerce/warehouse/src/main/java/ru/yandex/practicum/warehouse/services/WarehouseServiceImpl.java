85a051016ab9ddaf9ced5d2116303a262bb706c3
Y7tion <farnitin@yandex.ru>
2025-12-20 00:14:43 +0400

    add WarehouseServiceImpl

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/services/WarehouseServiceImpl.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/services/WarehouseServiceImpl.java
@@ -1,14 +1,20 @@
 package ru.yandex.practicum.warehouse.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.interaction_api.model.dto.AddressDto;
 import ru.yandex.practicum.interaction_api.model.dto.BookedProductsDto;
 import ru.yandex.practicum.interaction_api.model.dto.ShoppingCartDto;
 import ru.yandex.practicum.interaction_api.model.dto.request.AddProductToWarehouseRequest;
 import ru.yandex.practicum.interaction_api.model.dto.request.NewProductInWarehouseRequest;
+import ru.yandex.practicum.warehouse.clients.ShoppingCartClient;
+import ru.yandex.practicum.warehouse.clients.ShoppingStoreClient;
 
 @Service
+@RequiredArgsConstructor
 public class WarehouseServiceImpl implements WarehouseService{
+    private final ShoppingCartClient cartClient;
+    private final ShoppingStoreClient storeClient;
 
     public AddressDto getAddress() {
         return new AddressDto();
