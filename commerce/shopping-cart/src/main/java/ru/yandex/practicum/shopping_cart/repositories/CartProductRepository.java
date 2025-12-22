ad3d2d9b94b5cbed3d22e33cb1a55a56384d3555
Y7tion <farnitin@yandex.ru>
2025-12-22 23:35:11 +0400

    delete annotation Query

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/repositories/CartProductRepository.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/repositories/CartProductRepository.java
@@ -1,17 +1,19 @@
 package ru.yandex.practicum.shopping_cart.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import ru.yandex.practicum.interaction_api.model.entity.helpers.CartProductId;
 import ru.yandex.practicum.interaction_api.model.entity.CartProductEntity;
+import ru.yandex.practicum.interaction_api.model.entity.helpers.CartProductId;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartProductRepository extends JpaRepository<CartProductEntity, CartProductId> {
-    @Query("SELECT cp FROM CartProductEntity cp WHERE cp.shoppingCart.id = :cartId")
-    List<CartProductEntity> findByCartId(@Param("cartId") String cartId); // нахождение корзины по id корзины
 
-    @Query("SELECT COALESCE(SUM(c.quantity), 0) FROM CartProductEntity c WHERE c.shoppingCart.id = :cartId")
-    Integer getTotalItemsCount(@Param("cartId") String cartId); // нахождение количества товара в корзине
+    List<CartProductEntity> findByCartId(String cartId);
+
+    Integer getTotalItemsCount(String cartId);
+
+    Optional<CartProductEntity> findByShoppingCart_IdAndProductId(String shoppingCartId, String productId);
+
+    void deleteByShoppingCart_IdAndProductId(String shoppingCartId, String productId);
 }
