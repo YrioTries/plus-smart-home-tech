dc6a1758d29346ca716231d29ae5e45c47ad4a78
Y7tion <farnitin@yandex.ru>
2025-12-22 11:36:49 +0400

    add Feign annotation

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/ShoppingCartApplication.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/ShoppingCartApplication.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.shopping_cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ShoppingCartApplication {
     public static void main(String[] args) {
--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping_store/ShoppingStoreApplication.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping_store/ShoppingStoreApplication.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.shopping_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ShoppingStoreApplication {
     public static void main(String[] args) {
