bb5ba292c845e473c211ec4511ba180c289f1801
Y7tion <farnitin@yandex.ru>
2025-12-23 20:21:30 +0400

    add setter annotation in ProductDto

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction_api/model/dto/ProductDto.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction_api/model/dto/ProductDto.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.interaction_api.model.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import ru.yandex.practicum.interaction_api.enums.ProductCategory;
 import ru.yandex.practicum.interaction_api.enums.ProductState;
 import ru.yandex.practicum.interaction_api.enums.QuantityState;
 
 @Getter
+@Setter
 public class ProductDto {
     private String productId;
     private String productName;
