032d7523496710d45ad0b54df0c88f34d97bba2f
Y7tion <farnitin@yandex.ru>
2025-12-23 19:44:07 +0400

    systemize annotations

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/ShoppingCartApplication.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/shopping_cart/ShoppingCartApplication.java
@@ -2,9 +2,10 @@ package ru.yandex.practicum.shopping_cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interaction_api.clients")
 public class ShoppingCartApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
