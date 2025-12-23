c34b01a08d5b4ec2e1968a0c9b2ca93bba07760c
Y7tion <farnitin@yandex.ru>
2025-12-23 11:06:28 +0400

    add warehouse schema data base

---
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS warehouse_products (
+    product_id VARCHAR(36)      NOT NULL,
+    fragile    BOOLEAN          NOT NULL,
+    width      DOUBLE PRECISION NOT NULL,
+    height     DOUBLE PRECISION NOT NULL,
+    depth      DOUBLE PRECISION NOT NULL,
+    weight     DOUBLE PRECISION NOT NULL,
+    quantity   INTEGER          NOT NULL,
+    CONSTRAINT pk_warehouse_products PRIMARY KEY (product_id)
+);
