fab4d4f28e22f7c9c50c2155300869bc339d6418
Y7tion <farnitin@yandex.ru>
2025-12-29 11:16:09 +0400

    fix Pageable

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping_store/repositories/ProductRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping_store/repositories/ProductRepository.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.shopping_store.repositories;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.interaction_api.enums.ProductCategory;
 import ru.yandex.practicum.interaction_api.enums.ProductState;
-import ru.yandex.practicum.interaction_api.model.dto.Pageable;
 import ru.yandex.practicum.shopping_store.entity.ProductEntity;
 
 import java.util.List;
